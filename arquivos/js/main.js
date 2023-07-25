be8901b43b40bbfb3ad18a5b69dd90796401a8a7
Thales Mariano <thales.mariano125@gmail.com>
2023-07-25 14:48:59 -0300

    script added

---
+++ arquivos/js/main.js
@@ -0,0 +1,7 @@
+
+const nextForm = document.querySelector("#next-form")
+const inputs_1 = document.querySelectorAll(".inputs-1")
+
+nextForm.addEventListener("submit", e => {
+	e.preventDefault()
+})
