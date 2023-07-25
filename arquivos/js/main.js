5f175ac599796baec298f7faff297d0f117687ed
Thales Mariano <thales.mariano125@gmail.com>
2023-07-25 20:34:55 -0300

    icons show on invalid or valid input

--- arquivos/js/main.js
+++ arquivos/js/main.js
@@ -12,13 +12,18 @@ inputs.forEach(i => {
 
 function verifyInput(){
 	inputs.forEach(input => {
-		const warnText = input.parentNode.parentNode.children[1]
+		const warnText = input.parentNode.parentNode.parentNode.children[1]
+		const warnIcon = input.parentNode.children[0]
 
 		if(input.value == "" || input.value == " "){
 			warnText.classList.remove("opacity-0")
+			warnIcon.classList.remove("opacity-0")
+			warnIcon.src = "arquivos/images/invalid.png"
+
 			input.dataset.valid = "false"
 		}else{
 			warnText.classList.add("opacity-0")
+			warnIcon.src = "arquivos/images/valid.png"
 
 			if(input.type == "email"){
 				if(emailRegex.test(input.value)){
